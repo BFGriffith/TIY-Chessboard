91342d0506c81ea156b81ed48ae3366002c90f0e
Epicurean306 <benjaminforbesgriffith@gmail.com>
2015-05-31 15:38:06 -0400

    jQuery 1st move, fixed stupid piece-styling to starting squares!

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
 (function(window) {
-    'use strict';
+    /*
     window.chess = {
 
     }
-
+    */
+alert("jQuery?");
+    $('.NextMove').click(function() {
+      $('.rank-2 > .file-d').removeClass('WhitePawn');
+      $('.rank-4 > .file-d').addClass('WhitePawn');
+      console.log("Click:BOOM!");
+});
+/*
     _.forEach(..., function(element, ...) {
       element.addEventListener('click', function() {
         console.log("This works for all of the buttons!!");
@@ -11,5 +18,5 @@
       });
 
     });
-
+*/
 })(window);
