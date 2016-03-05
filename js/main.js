efdda532c1ee79c868be761182af9e1f2494a932
Epicurean306 <benjaminforbesgriffith@gmail.com>
2016-03-05 16:35:09 -0500

    chessboard

--- js/main.js
+++ js/main.js
@@ -1,27 +1,67 @@
 (function(window) {
-  $('.NextMove').click(function() {
-      chess.move(chess.game.move1);
-  });
 
-    $('.lastMove').click(function() {
-        chess.move(chess.game.move1);
-        chess.move(chess.game.move2);
-        chess.move(chess.game.move3);
-        chess.move(chess.game.move4);
-        chess.move(chess.game.move5);
-        chess.move(chess.game.move6);
-        chess.move(chess.game.move7);
-        chess.move(chess.game.move8);
-        chess.move(chess.game.move9);
-    });
+        /**
+         * What does NextMove do?
+         *
+         * @param {Number} fromRank
+         * @param {String} fromFile
+         * @param {Number} fromRank
+         * @param {String} fromFile
+         * @param {Class} piece remove/addClass
+         * @return {}
+         */
 
-    /*
-    window.chess = {
 
-    }
-    */
+        function(window) {
+            $('.NextMove').click(function() {
+                    NextMove(2, 'd', 4, 'd', 'WhitePawn')
+                    NextMove(8, 'g', 6, 'f', 'BlackKnightG')
+                };
 
-    /*
+                function NextMove(fromRank, fromFile, toRank, toFile, piece) {
+                    $('.rank-' + fromRank + ' > fromFile').removeClass(piece);
+                    $(toRank + ' > ' + toFile).addClass(piece);
+                };
+
+                $('.last').click(function() {
+                    for (chess.counter; chess.counter < chess.game.length; chess.counter++) {
+                        chess.NextMove(chess.game[i]);
+                    };
+                });
+
+
+                $('.NextMove').click(function() {
+                    //chess.counter = (chess.counter + 1) % chess.game.length;
+                    //Stack Overflow
+                    if (chess.counter < chess.game.length) {
+                        chess.move(chess.game[chess.counter]);
+                        chess.counter = chess.counter + 1;
+                    }
+                });
+
+                // $('.NextMove').click(function() {
+                //     chess.move(chess.game.move1);
+                // });
+
+                $('.lastMove').click(function() {
+                    chess.move(chess.game.move1);
+                    chess.move(chess.game.move2);
+                    chess.move(chess.game.move3);
+                    chess.move(chess.game.move4);
+                    chess.move(chess.game.move5);
+                    chess.move(chess.game.move6);
+                    chess.move(chess.game.move7);
+                    chess.move(chess.game.move8);
+                    chess.move(chess.game.move9);
+                });
+
+                /*
+                window.chess = {
+
+                }
+                */
+
+                /*
     $('.NextMove').click(function() {
       $('.rank-2 > .file-d').removeClass('WhitePawn');
       $('.rank-4 > .file-d').addClass('WhitePawn');
@@ -36,4 +76,4 @@
 
     });
 */
-})(window);
+            })(window);
--- js/models.js
+++ js/models.js
@@ -1,27 +1,47 @@
 (function(window) {
 
-var game = {
-  move1: ['.rank-2', '.file-d', '.rank-4', '.file-d', 'WhitePawn'],
-  move2: ['.rank-8', '.file-g', '.rank-6', '.file-f', 'BlackKnightG'],
-  move3: ['.rank-2', '.file-c', '.rank-4', '.file-c', 'WhitePawn'],
-  move4: ['.rank-7', '.file-e', '.rank-6', '.file-e', 'BlackPawn'],
-  move5: ['.rank-2', '.file-g', '.rank-3', '.file-g', 'WhitePawn'],
-  move6: ['.rank-7', '.file-d', '.rank-5', '.file-d', 'BlackPawn'],
-  move7: ['.rank-1', '.file-f', '.rank-5', '.file-g', 'WhiteBishopF'],
-  move8: ['.rank-8', '.file-f', '.rank-7', '.file-e', 'WhitePawn'],
-  move9: ['.rank-1', '.file-g', '.rank-3', '.file-f', 'WhiteKnightG']
-};
-function move (whichMove){
-  $(whichMove[0] + ' > ' + whichMove[1]).removeClass(whichMove[4]);
-  $(whichMove[2] + ' > ' + whichMove[3]).addClass(whichMove[4]);
-};
+    var game = [ //list of 5-tuples: fromRank, fromFile, toRank, toFile, piece
+        ['.rank-2', '.file-d', '.rank-4', '.file-d', 'whitepawn'],
+        ['.rank-8', '.file-g', '.rank-6', '.file-f', 'blackknight'],
+        ['.rank-2', '.file-c', '.rank-4', '.file-c', 'whitepawn'],
+        ['.rank-7', '.file-e', '.rank-6', '.file-e', 'blackpawn'],
+        ['.rank-2', '.file-g', '.rank-3', '.file-g', 'whitepawn'],
+        ['.rank-7', '.file-d', '.rank-5', '.file-d', 'blackpawn'],
+        ['.rank-1', '.file-f', '.rank-2', '.file-g', 'whitebishop'],
+        ['.rank-8', '.file-f', '.rank-7', '.file-e', 'blackbishop'],
+        ['.rank-1', '.file-g', '.rank-3', '.file-f', 'whiteknight']
+    ];
 
+    var reverseGame = [
+        ['.rank-3', '.file-f', '.rank-1', '.file-g', 'whiteknight'],
+        ['.rank-7', '.file-e', '.rank-8', '.file-f', 'blackbishop'],
+        ['.rank-2', '.file-g', '.rank-1', '.file-f', 'whitebishop'],
+        ['.rank-5', '.file-d', '.rank-7', '.file-d', 'blackpawn'],
+        ['.rank-3', '.file-g', '.rank-2', '.file-g', 'whitepawn'],
+        ['.rank-6', '.file-e', '.rank-7', '.file-e', 'blackpawn'],
+        ['.rank-4', '.file-c', '.rank-2', '.file-c', 'whitepawn'],
+        ['.rank-6', '.file-f', '.rank-8', '.file-g', 'blackknight'],
+        ['.rank-4', '.file-d', '.rank-2', '.file-d', 'whitepawn']
+    ];
 
-window.chess = {
-"game": game,
-"move": move,
-};
-  })(window);
+    var counter = -1;
+    /**
+    * move this function into main.js
+    * @param {Array} whichMove
+    * @return {}
+    */
+    function NextMove(whichMove) {
+        $(whichMove[0] + ' > ' + whichMove[1]).removeClass(whichMove[4]);
+        $(whichMove[2] + ' > ' + whichMove[3]).addClass(whichMove[4]);
+    };
+
+    window.chess = {
+        "game": game,
+        "reverseGame": reverseGame,
+        "NextMove": NextMove,
+        "counter": counter
+    };
+})(window);
 
 // (function(window) {
 //     'use strict';
